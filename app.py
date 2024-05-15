f78ca9fcdc64aedda6c7a521ad33eeefcb58c653
Rodriasd <veronrodrigo98@gmail.com>
2024-05-15 10:23:27 -0300

    was captured the labels 'a'

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
-response = requests.get("https://pypi.org");
+response = requests.get("https://pypi.org")
 
-print(response.text)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+results = soup.find_all('a')
+
+for a in results:
+    print(a)
